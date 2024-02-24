11b4e56e0bf249975598f85f7c394a7838dfd19b
Dannydramo <alumandudaniel217@gmail.com>
2024-02-24 06:39:09 -0800

    [fix]: added frontend domain to cors

--- app.js
+++ app.js
@@ -12,7 +12,19 @@ const AppError = require("./utils/appError");
 const globalErorHandler = require("./controllers/errorController");
 const app = express();
 
-app.use(cors());
+const allowedOrigins = ["https://scribbles-snowy.vercel.app"];
+
+app.use(
+    cors({
+        origin: function (origin, callback) {
+            if (!origin || allowedOrigins.includes(origin)) {
+                callback(null, true);
+            } else {
+                callback(new Error("Not allowed by CORS"));
+            }
+        },
+    })
+);
 app.use(helmet());
 app.use(express.json());
 app.use(mongoSanitize());
