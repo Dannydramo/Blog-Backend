e0772d87198b2e7caf333edacf5373cb6d4e35cf
Dannydramo <alumandudaniel217@gmail.com>
2024-03-05 01:20:08 -0800

    [feat]: added nodemailer for sending email

--- utils/email.js
+++ utils/email.js
@@ -2,20 +2,15 @@ const nodemailer = require("nodemailer");
 
 const sendEmail = async (options) => {
     const transporter = nodemailer.createTransport({
-        pool: true,
         host: process.env.EMAIL_HOST,
         port: process.env.EMAIL_PORT,
-        secure: true,
         auth: {
             user: process.env.EMAIL_USERNAME,
             pass: process.env.EMAIL_PASSWORD,
         },
-        tls: {
-            rejectUnauthorized: false,
-        },
     });
     const mailOptions = {
-        from: "scribbles@gmail.com",
+        from: process.env.EMAIL_FROM,
         to: options.email,
         subject: options.subject,
         html: options.message,
